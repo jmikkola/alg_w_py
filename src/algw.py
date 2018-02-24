8f62cd5e3cbba38bd44b3e2bb681020343b0dbb3
Jeremy Mikkola <jeremy@jeremymikkola.com>
2018-02-24 10:10:46 -0800

    Annotate with what I've figured out

--- src/algw.py
+++ src/algw.py
@@ -14,6 +14,10 @@ class EVar(Expression):
     def get_type(self, tenv, type_inference):
         if self.name in tenv:
             sigma = tenv[self.name]
+            # No substitution needed, the type for this has already
+            # been substituted in wherever needed.
+            # Instantiate the type scheme so that this can be a subtype
+            # of the actual type.
             return {}, sigma.instantiate(type_inference)
         raise Exception('unbound variable: ' + self.name)
 
@@ -38,10 +42,18 @@ class EAbs(Expression):
         return '(lambda [{x}] {expr})'.format(x=self.x, expr=self.expr.show())
 
     def get_type(self, tenv, type_inference):
+        # Add the variable "x" to the type environment,
+        # just bound to a new typevar.
         tvar = TVar(type_inference.new_type_var())
         tenv0 = tenv.remove(self.x)
         tenv1 = tenv0.add(self.x, Scheme([], tvar))
+
+        # Use that environment to get the type of the function body
         sub1, type1 = self.expr.get_type(tenv1, type_inference)
+
+        # Typing the function body may restrict the type of the argument
+        # to more specific than just a var, so apply the substitution
+        # to that argument before building the function type.
         return sub1, TFunc(tvar.apply_sub(sub1), type1)
 
 
@@ -54,13 +66,30 @@ class EApp(Expression):
         return '(' + self.e1.show() + ' ' + self.e2.show() + ')'
 
     def get_type(self, tenv, type_inference):
-        tvar = TVar(type_inference.new_type_var())
+        # Making a generic instance of the function's type here isn't necessary
+        # because that already happens when typing the variable that evaluates
+        # to the function.
+
+        # First, find the type of the function.
         sub1, type1 = self.e1.get_type(tenv, type_inference)
+        # Finding that type may have resulted in substitutions that apply
+        # to the argument (e.g. if the function closes over a variable that is
+        # also used in the argument expression), so apply that substitution
+        # to the type env before getting the type of the argument.
         sub2, type2 = self.e2.get_type(tenv.apply_sub(sub1), type_inference)
+
+        # We use tvar to represent the return type of the function in this
+        # particular instance.
+        # sub2 is applied to type1 in case that further narrows the type.
+        tvar = TVar(type_inference.new_type_var())
         sub3 = type_inference.most_general_unifier(
             type1.apply_sub(sub2), TFunc(type2, tvar)
         )
+
+        # The resulting substitution needs to take all three substitutions
+        # into account.
         sub = compose_subs(sub3, compose_subs(sub2, sub1))
+        # Apply sub3 to tvar to find out what the type actually is
         return sub, tvar.apply_sub(sub3)
 
 
@@ -75,12 +104,23 @@ class ELet(Expression):
             x=self.x, e1=self.e1.show(), e2=self.e2.show())
 
     def get_type(self, tenv, type_inference):
+        # First, get the type of the first expression in the current
+        # environment (which may have a different binding for x,
+        # so we don't remove x from tenv just yet).
         sub1, type1 = self.e1.get_type(tenv, type_inference)
 
+        # Apply sub1 to create a new type env with what we just learned,
+        # and use that to figure out how to generalize type1 into a
+        # type scheme.
         type1_sub = tenv.apply_sub(sub1).generalize(type1)
+        # Create a new type environment where x is bound to that type scheme.
         tenv1 = tenv.add(self.x, type1_sub)
 
+        # Use that new type env (with sub1 applied to it) to find the type
+        # of the inner expression.
         sub2, type2 = self.e2.get_type(tenv1.apply_sub(sub1), type_inference)
+        # The resulting substitution takes both of the other substitutions
+        # into account.
         return compose_subs(sub1, sub2), type2
 
 
@@ -177,14 +217,30 @@ class TFunc(Type):
 
 
 class Scheme:
+    '''
+    A type scheme is just a type with some extra information
+    (a set of type variables) attached.
+
+    That set of type variables influnces the free_type_variables
+    and apply_sub operations, and makes the instantiate operation
+    possible.
+    '''
     def __init__(self, bound, t):
         self.bound = set(bound)
         self.t = t
 
     def free_type_variables(self):
+        '''
+        This is every free type variable in the type
+        that doesn't appear in the bound set.
+        '''
         return self.t.free_type_variables() - self.bound
 
     def apply_sub(self, sub):
+        '''
+        This applies a substitution to the type,
+        skipping any substitution of a variable in the bound set.
+        '''
         smaller_sub = {
             v: t
             for v, t in sub.items()
@@ -194,6 +250,14 @@ class Scheme:
         return Scheme(self.bound, ts)
 
     def instantiate(self, type_inference):
+        '''
+        This creates a new polymorphic instance of the type.
+
+        For example, if the type is (a -> a), this would
+        create (b -> b), allowing it to be unified against
+        (int -> c) to find that b and c are int without
+        changing the type of a.
+        '''
         sub = {
             var: TVar(type_inference.new_type_var())
             for var in self.bound
@@ -212,22 +276,35 @@ def compose_subs(s1, s2):
 
 
 class TypeEnv:
+    '''
+    A type environment is a mapping from variable name
+    (variable in the program, not type variable) to
+    type schemes.
+    '''
     def __init__(self, schemes):
         self.schemes = schemes
 
     def free_type_variables(self):
+        '''
+        Returns the union of all the free type variables
+        for each scheme.
+        '''
         ftvs = set()
         for scheme in self.schemes.values():
             ftvs = ftvs | scheme.free_type_variables()
         return ftvs
 
     def apply_sub(self, sub):
+        '''
+        Applies a substitution to all the schemes.
+        '''
         return TypeEnv({
             v: scheme.apply_sub(sub)
             for v, scheme in self.schemes.items()
         })
 
     def remove(self, var):
+        ''' Removes a binding '''
         return TypeEnv({
             v: scheme
             for v, scheme in self.schemes.items()
@@ -235,11 +312,16 @@ class TypeEnv:
         })
 
     def add(self, var, scheme):
+        ''' Adds a binding '''
         copy = {v: s for v, s in self.schemes.items()}
         copy[var] = scheme
         return TypeEnv(copy)
 
     def generalize(self, t):
+        '''
+        Generalizes the type t into a type scheme by looking at what
+        type variables are free in the type and not in the environment.
+        '''
         bound_vars = t.free_type_variables() - self.free_type_variables()
         return Scheme(bound_vars, t)
 
@@ -255,11 +337,17 @@ class TypeInference:
         self._next_tvar_n = 0
 
     def new_type_var(self):
+        ''' Generates a new type variable on each call '''
         tvar = 'tv_' + str(self._next_tvar_n)
         self._next_tvar_n += 1
         return tvar
 
     def most_general_unifier(self, t1, t2):
+        '''
+        Either finds a substitution such that
+        t1.apply_sub(sub) == t2.apply_sub(sub)
+        or throws an exception.
+        '''
         if isinstance(t1, TFunc) and isinstance(t2, TFunc):
             s1 = self.most_general_unifier(t1.left, t2.left)
             s2 = self.most_general_unifier(
@@ -279,6 +367,11 @@ class TypeInference:
             raise Exception('cannot unify {} and {}'.format(t1, t2))
 
     def var_bind(self, name, t):
+        '''
+        Creates a substitution from name to t
+        after checking that name isn't free in t
+        (which would lead to an infinite type)
+        '''
         if not isinstance(t, Type):
             raise Exception(t)
         if isinstance(t, TVar) and t.name == name:
